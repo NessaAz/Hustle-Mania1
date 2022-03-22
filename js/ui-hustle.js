06d9f21a12a0c1fc34dffa2033925d032fc89305
vanta blanta <vantablanta@gmail.com>
2022-03-22 17:26:28 +0300

    empty boiler plates

--- js/ui-hustle.js
+++ js/ui-hustle.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
-    $(".book-button").on('click', function(){
-        // redirect to booking section
-    })
-
-    //sense input location and produce buttons with repective locations
+    
 })
