85c842ab7e34da3f7eef4c73fa8177c52b3dfe6e
vanta blanta <vantablanta@gmail.com>
2022-03-22 17:45:21 +0300

    booking and selection page

---
+++ js/ui-hustle.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".book-button").on('click', function(){
+        // redirect to booking section
+    })
+    
+    //sense input location and produce buttons with repective locations
+    
+})
