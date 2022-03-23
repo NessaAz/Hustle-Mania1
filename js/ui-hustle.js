210c532390c128121a777c6b9b33430fa5c6589a
vanta blanta <vantablanta@gmail.com>
2022-03-23 15:50:32 +0300

    adds a laptop json file, laptop selection form, and a senser for inputed laptop brand

--- js/ui-hustle.js
+++ js/ui-hustle.js
@@ -2,10 +2,47 @@ $(document).ready(function(){
     $(".book-button").on('click', function(){
         // redirect to booking section
     })
+
+     //sense input location and produce buttons with repective locations
+
+    $.ajax({
+        url: 'js/locations.json',
+        success:function(result){
+            for(let location of result){
+               console.log(location.mombasa)             
+              
+            }
+        },
+    })
+
+    $.ajax({
+        url: 'js/laptops.json',
+        success:function(result){
+            for(let laptop of result){
+               console.log(laptop.dell)             
+              
+            }
+        },
+    })
+
+
+
+
+
+
+   
+
     
-    //sense input location and produce buttons with repective locations
-    $('select').on('change', function() {
-        alert( this.value );
-      });
+
+
+
+
+
+
+
+    
+    
+  
+   
     
 })
