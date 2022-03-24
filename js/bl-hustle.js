33b92f7deca17b2932ce8eafb39190caf0e26883
vanta blanta <vantablanta@gmail.com>
2022-03-24 03:42:37 +0300

    adds specific locations upon select of locations, adds specific laptops buttons aupon select of brands

--- js/bl-hustle.js
+++ js/bl-hustle.js
@@ -1,5 +1,16 @@
 
 
-let mombasaLocations = new Array
+let mombasa = ["Nyali","Ukunda","Bamburi","Diani","Shanzu"]
+let nairobi = ["Westlands" ,"South C", "Eastlands","Thika Rd","CBD"]
+let kiambu=  ["Juja","Ruaka","Kikuyu","Gachie","Banana"]
+let kericho= ["Kabianga","Kapsoit","Jamji", "Kiptere","Sondu"]
+let kisumu =  ["Ahero","Koru","Ndori","Rambara","Nango"]
 
-// loop  through json object storing mombasa locations 
+let dell = ["IdeaPad", "Legion", "ThinkPad", "ThinkBook", "Yoga"]
+let hp = ["Elitebook", "Envy", "Omen", "Pavilion", "ZBook"]
+let apple = ["Alienware", "G Series", "Inspiron", "Latitude", "Precision"]
+let lenovo = ["MacBook", "MacBook Air", "MacBook Pro", "iPad"]
+let asus = ["ExpertBook", "Pro", "ProArt", "Transformer", "TUF"]
+ 
+
+
--- js/ui-hustle.js
+++ js/ui-hustle.js
@@ -1,52 +1,144 @@
-$(document).ready(function(){
-    $(".book-button").on('click', function(){
+$(document).ready(function () {
+    $(".book-button").on('click', function () {
         // redirect to booking section
     })
 
-     //sense input location and produce buttons with repective locations
+    //sense input location and produce buttons with repective locations
+    $("#inputCity").change(function () {
+        $(".location-form").submit()
+        let selected = $(".location:selected").text()
+        if (selected == "Mombasa") {
+            resetLocations()
+            mombasa.forEach(element => {
+                $(".buttonsLocate").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            });
+        } else if (selected == "Nairobi") {
+            resetLocations()
+            nairobi.forEach(element => {
+                $(".buttonsLocate").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
 
-    $.ajax({
-        url: 'js/locations.json',
-        success:function(result){
-            for(let location of result){
-               console.log(location.mombasa)             
-              
-            }
-        },
+            })
+        } else if (selected == "Kiambu") {
+            resetLocations()
+            kiambu.forEach(element => {
+                $(".buttonsLocate").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+
+            })
+        } else if (selected == "Kericho") {
+            resetLocations()
+            kericho.forEach(element => {
+                $(".buttonsLocate").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+
+            })
+        } else if (selected == "Kisumu") {
+            resetLocations()
+            kisumu.forEach(element => {
+                $(".buttonsLocate").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            })
+        } else {
+            resetLocations()
+        }
+
     })
 
-    $.ajax({
-        url: 'js/laptops.json',
-        success:function(result){
-            for(let laptop of result){
-               console.log(laptop.dell)             
-              
-            }
-        },
-    
+
+    function resetLocations() {
+        $(".buttonsLocate").empty()
+    }
+    function resetLaptops() {
+        $(".buttonsLaptop").empty()
+    }
+
+
+    $("#inputLaptop").change(function () {
+        $(".laptop-form").submit()
+        let selected = $(".laptop:selected").text()
+        if (selected == "Dell") {
+            resetLaptops()
+            dell.forEach(element => {
+                $(".buttonsLaptop").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            });
+        } else if (selected == "Apple") {
+            resetLaptops()
+            apple.forEach(element => {
+                $(".buttonsLaptop").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+
+            })
+        } else if (selected == "Hp") {
+            resetLaptops()
+            hp.forEach(element => {
+                $(".buttonsLaptop").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            })
+        } else if (selected == "Asus") {
+            resetLaptops()
+            asus.forEach(element => {
+                $(".buttonsLaptop").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            })
+        } else if (selected == "Lenovo") {
+            resetLaptops()
+            lenovo.forEach(element => {
+                $(".buttonsLaptop").append(
+                    `<button type="button" class="m-2 location-button btn btn-secondary rounded-pill">
+                    ${element}</button>`
+                )
+            })
+        } else{
+            resetLaptops()
+        }
+
     })
 
-    
 
 
-
-
-
-
-   
-
-    
-
-
-
-
-
-
-
-    
-    
-  
-   
-    
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
